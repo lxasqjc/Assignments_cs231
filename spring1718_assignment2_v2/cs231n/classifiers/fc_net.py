8f62d191a445bedb9cb855afefe6fcebe07eb397
lxasqjc <jinchen0227@gmail.com>
2018-08-28 15:31:20 +0100

    Revert "sandwich affine layer"
    This reverts commit bda0836613719a9a1d575d9400ccb8b38308399f.

--- spring1718_assignment2_v2/cs231n/classifiers/fc_net.py
+++ spring1718_assignment2_v2/cs231n/classifiers/fc_net.py
@@ -47,10 +47,7 @@ class TwoLayerNet(object):
         # and biases using the keys 'W1' and 'b1' and second layer                 #
         # weights and biases using the keys 'W2' and 'b2'.                         #
         ############################################################################
-        self.params['W1'] = weight_scale * (np.random.randn(input_dim, hidden_dim) - 0.5)
-        self.params['b1'] = np.zeros(hidden_dim)
-        self.params['W2'] = weight_scale * (np.random.randn(hidden_dim, num_classes) - 0.5)
-        self.params['b2'] = np.zeros(num_classes)
+        pass
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -80,9 +77,7 @@ class TwoLayerNet(object):
         # TODO: Implement the forward pass for the two-layer net, computing the    #
         # class scores for X and storing them in the scores variable.              #
         ############################################################################
-        out1, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
-        scores, cache2 = affine_relu_forward(out1, self.params['W2'], self.params['b2'])
-        
+        pass
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -102,16 +97,7 @@ class TwoLayerNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        loss, dscore = softmax_loss(scores, y)
-
-        loss += 0.5 * self.reg * np.sum(self.params['W1'] ** 2) + \
-                0.5 * self.reg * np.sum(self.params['W2'] ** 2)
-        
-        dhidden, grads['W2'], grads['b2'] = affine_relu_backward(dscore, cache2)
-        _      , grads['W1'], grads['b1'] = affine_relu_backward(dhidden, cache1) 
-        grads['W2'] += self.reg * self.params['W2']
-        grads['W1'] += self.reg * self.params['W1']
-        
+        pass
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
